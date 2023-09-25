736f2f907d0825a62962017ebbbb37c99e386d36
jangheepark <jeanparkmk@gmail.com>
2023-09-25 19:00:53 +0900

    [api-server-v1] feat: TextBlockService 클래스 (#21)

--- api/server-v1/src/main/java/kr/joberchip/server/v1/space/block/service/TextBlockService.java
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/space/block/service/TextBlockService.java
@@ -1,8 +1,24 @@
 package kr.joberchip.server.v1.space.block.service;
 
+import kr.joberchip.core.space.block.TextBlock;
+import kr.joberchip.server.v1.space.block.repository.TextBlockRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
-public class TextBlockService {}
+@RequiredArgsConstructor
+public class TextBlockService {
+    private final TextBlockRepository textBlockRepository;
+
+    @Transactional
+    public TextBlock createTextBlock(String content) {
+        TextBlock textBlock = new TextBlock();
+        textBlock.setContent(content);
+        return textBlockRepository.save(textBlock);
+    }
+
+
+}
