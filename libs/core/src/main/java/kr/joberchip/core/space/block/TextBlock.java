cd7ae94374b6228b05a7269bef171a89ffcf701b
jangheepark <jeanparkmk@gmail.com>
2023-09-25 18:49:37 +0900

    [api-server-v1] feat: TextBlock > getTextBlockId 메서드 추가(#21)

--- libs/core/src/main/java/kr/joberchip/core/space/block/TextBlock.java
+++ libs/core/src/main/java/kr/joberchip/core/space/block/TextBlock.java
@@ -5,11 +5,22 @@ import javax.persistence.Entity;
 import javax.persistence.Lob;
 import javax.persistence.Table;
 import kr.joberchip.core.space.BaseObject;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
 
 @Entity
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "text_block_tb")
 public class TextBlock extends BaseObject {
-  @Lob
   @Column(name = "content")
   private String content;
+
+  public UUID getTextBlockId() {
+    return this.objectId;
+  }
 }
