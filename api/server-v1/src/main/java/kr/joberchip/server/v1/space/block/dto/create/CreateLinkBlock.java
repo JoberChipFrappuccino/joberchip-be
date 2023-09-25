8c9ab246841a5e174d4d25417ea9b87e414fed30
jangheepark <jeanparkmk@gmail.com>
2023-09-25 19:27:23 +0900

    [api-server-v1] feat: CreateLinkBlock dto 클래스 (#50)

--- api/server-v1/src/main/java/kr/joberchip/server/v1/space/block/dto/create/CreateLinkBlock.java
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/space/block/dto/create/CreateLinkBlock.java
@@ -1,3 +1,16 @@
 package kr.joberchip.server.v1.space.block.dto.create;
 
-public class CreateLinkBlock {}
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateLinkBlock {
+    private String title;
+    private String description;
+    private String link;
+}
