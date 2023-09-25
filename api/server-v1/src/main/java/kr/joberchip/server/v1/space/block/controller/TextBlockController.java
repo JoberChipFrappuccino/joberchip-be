eb0e2e968ec822b79da54b4d5d694bf2297a24da
jangheepark <jeanparkmk@gmail.com>
2023-09-25 18:59:45 +0900

    [api-server-v1] feat: TextBlockController 클래스 (#21)

--- api/server-v1/src/main/java/kr/joberchip/server/v1/space/block/controller/TextBlockController.java
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/space/block/controller/TextBlockController.java
@@ -1,6 +1,8 @@
 package kr.joberchip.server.v1.space.block.controller;
 
+import kr.joberchip.core.space.block.TextBlock;
 import kr.joberchip.server.v1._utils.ApiResponse;
+import kr.joberchip.server.v1.space.block.service.TextBlockService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -12,13 +14,17 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/v1/space/{spaceId}/page/{pageId}/block")
 public class TextBlockController {
 
+  private final TextBlockService textBlockService;
+
   @PostMapping("/text")
   public ApiResponse.Result<Object> createTextBlock(
       @PathVariable Long spaceId,
       @PathVariable Long pageId,
       @RequestBody String content) {
 
-    return ApiResponse.success();
+    TextBlock textBlock = textBlockService.createTextBlock(content);
+
+    return ApiResponse.success(textBlock);
   }
 
   @PutMapping("/text/{blockId}")
