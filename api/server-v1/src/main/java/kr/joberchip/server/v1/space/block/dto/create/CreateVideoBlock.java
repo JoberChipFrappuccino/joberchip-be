2805e1975ce81ecf89bd903f69a02ff527216dce
Horaiz-UQ <ekzmvlfxj@naver.com>
2023-09-25 12:14:38 +0900

    [api-auth-v1] feat: VideoBlock DTO 생성 (#18)

--- api/server-v1/src/main/java/kr/joberchip/server/v1/space/block/dto/create/CreateVideoBlock.java
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/space/block/dto/create/CreateVideoBlock.java
@@ -1,3 +1,15 @@
 package kr.joberchip.server.v1.space.block.dto.create;
 
-public class CreateVideoBlock {}
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Setter
+public class CreateVideoBlock {
+
+    private String title;
+    private String description;
+    private MultipartFile videoFile;
+
+}
--- api/server-v1/src/main/java/kr/joberchip/server/v1/space/block/dto/modify/ModifyVideoBlock.java
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/space/block/dto/modify/ModifyVideoBlock.java
@@ -1,3 +1,13 @@
 package kr.joberchip.server.v1.space.block.dto.modify;
 
-public class ModifyVideoBlock {}
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Setter
+public class ModifyVideoBlock {
+
+    private String title;
+    private MultipartFile file;
+}
