45a1d3e75263d6f85d160571fa0cdc8d31bd1907
Horaiz-UQ <ekzmvlfxj@naver.com>
2023-09-25 12:18:52 +0900

    [api-auth-v1] feat: VideoeController 추가 - 추후 수정 예정 (#18)

--- api/server-v1/src/main/java/kr/joberchip/server/v1/space/block/controller/VideoBlockController.java
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/space/block/controller/VideoBlockController.java
@@ -1,6 +1,8 @@
 package kr.joberchip.server.v1.space.block.controller;
 
 import kr.joberchip.server.v1._utils.ApiResponse;
+import kr.joberchip.server.v1.space.block.dto.create.CreateVideoBlock;
+import kr.joberchip.server.v1.space.block.service.VideoBlockService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -12,9 +14,18 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/v1/space/{spaceId}/page/{pageId}/block")
 public class VideoBlockController {
 
+  private final VideoBlockService videoBlockService;
+
   @PostMapping("/video")
   public ApiResponse.Result<Object> createVideoBlock(
-      @PathVariable Long spaceId, @PathVariable Long pageId, @RequestBody MultipartFile file) {
+      @PathVariable Long spaceId,
+      @PathVariable Long pageId,
+      @RequestParam MultipartFile videoFile) {
+
+    CreateVideoBlock createVideoBlock = new CreateVideoBlock();
+    createVideoBlock.setVideoFile(videoFile);
+
+    videoBlockService.uploadVideo(createVideoBlock);
 
     return ApiResponse.success();
   }
