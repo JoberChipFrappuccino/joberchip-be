799e194480e7607c5440c8f05bb2fe31ab0bfdae
jangheepark <jeanparkmk@gmail.com>
2023-09-25 19:24:33 +0900

    [api-server-v1] feat: LinkBlock 클래스 (#50)

--- libs/core/src/main/java/kr/joberchip/core/space/block/LinkBlock.java
+++ libs/core/src/main/java/kr/joberchip/core/space/block/LinkBlock.java
@@ -4,8 +4,16 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import kr.joberchip.core.space.BaseObject;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
 
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "link_block_tb")
 public class LinkBlock extends BaseObject {
   @Column(name = "title")
@@ -16,4 +24,8 @@ public class LinkBlock extends BaseObject {
 
   @Column(name = "link")
   private String link;
+
+  public UUID getLinkBlockId() {
+    return this.objectId;
+  }
 }
