231ae63faabd440f97cdba7e5248fc8dccc58a44
YongHo Shin <gdtknight@naver.com>
2023-09-26 14:26:11 +0900

    [api-server-v1] feat: TextBlockservice (#49)

--- api/server-v1/src/main/java/kr/joberchip/server/v1/share/block/service/TextBlockService.java
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/share/block/service/TextBlockService.java
@@ -1,7 +1,14 @@
 package kr.joberchip.server.v1.share.block.service;
 
-import kr.joberchip.core.space.block.TextBlock;
-import kr.joberchip.server.v1.space.block.repository.TextBlockRepository;
+import java.util.UUID;
+import javax.persistence.EntityExistsException;
+import kr.joberchip.core.share.block.TextBlock;
+import kr.joberchip.core.share.page.SharePage;
+import kr.joberchip.server.v1.share.block.dto.create.CreateTextBlock;
+import kr.joberchip.server.v1.share.block.dto.modify.ModifyTextBlock;
+import kr.joberchip.server.v1.share.block.dto.response.TextBlockResponse;
+import kr.joberchip.server.v1.share.block.repository.TextBlockRepository;
+import kr.joberchip.server.v1.share.page.repository.SharePageRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -11,14 +18,26 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class TextBlockService {
-    private final TextBlockRepository textBlockRepository;
+  private final SharePageRepository sharePageRepository;
+  private final TextBlockRepository textBlockRepository;
 
-    @Transactional
-    public TextBlock createTextBlock(String content) {
-        TextBlock textBlock = new TextBlock();
-        textBlock.setContent(content);
-        return textBlockRepository.save(textBlock);
-    }
+  @Transactional
+  public TextBlockResponse createTextBlock(UUID pageId, CreateTextBlock createTextBlock) {
+    TextBlock newTextBlock = createTextBlock.toEntity();
 
+    textBlockRepository.save(newTextBlock);
 
+    SharePage parentPage =
+        sharePageRepository.findById(pageId).orElseThrow(EntityExistsException::new);
+
+    parentPage.addTextBlock(newTextBlock);
+
+    return TextBlockResponse.fromEntity(newTextBlock);
+  }
+
+  public void modifyTextBlock(UUID pageId, UUID blockId, ModifyTextBlock modifyTextBlock) {
+  }
+
+  public void deleteTextBlock(UUID pageId, UUID blockId) {
+  }
 }
