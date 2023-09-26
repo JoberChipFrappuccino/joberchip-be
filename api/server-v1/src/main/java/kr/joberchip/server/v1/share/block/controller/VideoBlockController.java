0e6e8133235ac19840f9ab3b6b66f71d41d48e4f
YongHo Shin <gdtknight@naver.com>
2023-09-26 14:12:41 +0900

    [api-server-v1] feat: VideoBlockController (#49)
    - URI 수정
    - 추후 Validation 필요

--- api/server-v1/src/main/java/kr/joberchip/server/v1/share/block/controller/VideoBlockController.java
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/share/block/controller/VideoBlockController.java
@@ -1,41 +1,48 @@
 package kr.joberchip.server.v1.share.block.controller;
 
+import java.util.UUID;
 import kr.joberchip.server.v1._utils.ApiResponse;
-import kr.joberchip.server.v1.space.block.dto.create.CreateVideoBlock;
-import kr.joberchip.server.v1.space.block.service.VideoBlockService;
+import kr.joberchip.server.v1.share.block.dto.create.CreateVideoBlock;
+import kr.joberchip.server.v1.share.block.dto.modify.ModifyVideoBlock;
+import kr.joberchip.server.v1.share.block.service.VideoBlockService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/space/{spaceId}/page/{pageId}/block")
+@RequestMapping("/v1/page/{pageId}/videoBlock")
 public class VideoBlockController {
 
   private final VideoBlockService videoBlockService;
 
-  @PostMapping("/video")
+  @PostMapping
   public ApiResponse.Result<Object> createVideoBlock(
-      @PathVariable Long spaceId,
-      @PathVariable Long pageId,
-      @RequestParam MultipartFile videoFile) {
-
-    CreateVideoBlock createVideoBlock = new CreateVideoBlock();
-    createVideoBlock.setVideoFile(videoFile);
+      @PathVariable UUID pageId, @RequestParam CreateVideoBlock createVideoBlock) {
 
     videoBlockService.uploadVideo(createVideoBlock);
+    videoBlockService.createVideoBlock(pageId, createVideoBlock);
 
     return ApiResponse.success();
   }
 
-  @PutMapping("/video/{blockId}")
+  @PutMapping("/{blockId}")
   public ApiResponse.Result<Object> modifyVideoBlock(
-      @PathVariable Long spaceId,
-      @PathVariable Long pageId,
-      @PathVariable Long blockId,
-      @RequestBody MultipartFile file) {
+      @PathVariable UUID pageId,
+      @PathVariable UUID blockId,
+      @RequestBody ModifyVideoBlock modifyVideoBlock) {
+
+    videoBlockService.modifyVideoBlock(pageId, blockId, modifyVideoBlock);
+
+    return ApiResponse.success();
+  }
+
+  @DeleteMapping("/{blockId}")
+  public ApiResponse.Result<Object> deleteVideoBlock(
+      @PathVariable UUID pageId, @PathVariable UUID blockId) {
+
+    videoBlockService.deleteVideoBlock(pageId, blockId);
 
     return ApiResponse.success();
   }
