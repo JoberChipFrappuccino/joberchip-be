1a3661c605ac9a6b83ff1f2354592cf08cc586f7
YongHo Shin <gdtknight@naver.com>
2023-09-26 14:57:57 +0900

    [libs-core] fix: VideoBlockFile - import 정리, 코드 포맷팅 (#49)

--- libs/core/src/main/java/kr/joberchip/core/storage/VideoBlockFile.java
+++ libs/core/src/main/java/kr/joberchip/core/storage/VideoBlockFile.java
@@ -1,10 +1,9 @@
 package kr.joberchip.core.storage;
 
 import javax.persistence.*;
-import kr.joberchip.core.space.block.VideoBlock;
+import kr.joberchip.core.share.block.VideoBlock;
 import lombok.Getter;
 import lombok.Setter;
-import kr.joberchip.core.share.block.VideoBlock;
 
 @Entity
 @Table(name = "video_file_tb")
@@ -13,7 +12,7 @@ import kr.joberchip.core.share.block.VideoBlock;
 public class VideoBlockFile {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name="video_file_id")
+  @Column(name = "video_file_id")
   private Long videoFileId;
 
   @OneToOne private VideoBlock videoBlock;
