2d52f4dd9fa54822776a61ddd9d2ef436b0195e3
YongHo Shin <gdtknight@naver.com>
2023-09-26 14:50:28 +0900

    [api-server-v1] feat: VideoBlockService (#49)

--- api/server-v1/src/main/java/kr/joberchip/server/v1/share/block/service/VideoBlockService.java
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/share/block/service/VideoBlockService.java
@@ -6,87 +6,93 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.UUID;
-import kr.joberchip.core.space.block.VideoBlock;
+import kr.joberchip.core.share.block.VideoBlock;
 import kr.joberchip.core.storage.AttachedFile;
 import kr.joberchip.core.storage.VideoBlockFile;
-import kr.joberchip.server.v1.space.block.dto.create.CreateVideoBlock;
-import kr.joberchip.server.v1.space.block.repository.AttachedFileRepository;
-import kr.joberchip.server.v1.space.block.repository.VideoBlockRepository;
+import kr.joberchip.server.v1.share.block.dto.create.CreateVideoBlock;
+import kr.joberchip.server.v1.share.block.dto.modify.ModifyVideoBlock;
+import kr.joberchip.server.v1.share.block.repository.AttachedFileRepository;
+import kr.joberchip.server.v1.share.block.repository.VideoBlockRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class VideoBlockService {
 
-    @Autowired
-    private VideoBlockRepository videoBlockRepository;
+  private final VideoBlockRepository videoBlockRepository;
 
-    @Autowired
-    private AttachedFileRepository attachedFileRepository;
+  private final AttachedFileRepository attachedFileRepository;
 
-    @Value("${upload.directory}")
-    private String uploadDir;
+  @Value("${upload.directory}")
+  private String uploadDir;
 
-    @Value("${ffmpeg.executable}")
-    private String ffmpegPath;
+  @Value("${ffmpeg.executable}")
+  private String ffmpegPath;
 
-    // 영상 업로드
-    public void uploadVideo(CreateVideoBlock createVideoBlock) {
-        String originalFileName = createVideoBlock.getVideoFile().getOriginalFilename();
+  public void createVideoBlock(UUID pageId, CreateVideoBlock createVideoBlock) {}
 
-        String folderName = generateFolderName(originalFileName);
-        Path folderPath = Path.of(uploadDir, folderName);
+  // 영상 업로드
+  public void uploadVideo(CreateVideoBlock createVideoBlock) {
+    String originalFileName = createVideoBlock.videoFile().getOriginalFilename();
 
-        try {
-            if (!Files.exists(folderPath)) {
-                Files.createDirectories(folderPath);
-            }
+    String folderName = generateFolderName(originalFileName);
+    Path folderPath = Path.of(uploadDir, folderName);
 
-            String inputFilePath = folderPath.resolve(originalFileName).toString();
-            Files.copy(createVideoBlock.getVideoFile().getInputStream(), Paths.get(inputFilePath), StandardCopyOption.REPLACE_EXISTING);
+    try {
+      if (!Files.exists(folderPath)) {
+        Files.createDirectories(folderPath);
+      }
 
-            String outputFileName = UUID.randomUUID().toString() + ".mp4";
-            String outputFilePath = folderPath.resolve(outputFileName).toString();
+      String inputFilePath = folderPath.resolve(originalFileName).toString();
+      Files.copy(
+          createVideoBlock.videoFile().getInputStream(),
+          Paths.get(inputFilePath),
+          StandardCopyOption.REPLACE_EXISTING);
 
-            // ffmpeg를 사용하여 확장자를 변경
-            String[] ffmpegCommand = {
-                ffmpegPath,
-                "-i", inputFilePath,
-                "-c:v", "copy",
-                "-c:a", "aac",
-                outputFilePath
-            };
+      String outputFileName = UUID.randomUUID().toString() + ".mp4";
+      String outputFilePath = folderPath.resolve(outputFileName).toString();
 
-            ProcessBuilder pb = new ProcessBuilder(ffmpegCommand);
-            pb.inheritIO().start().waitFor();
+      // ffmpeg를 사용하여 확장자를 변경
+      String[] ffmpegCommand = {
+        ffmpegPath, "-i", inputFilePath, "-c:v", "copy", "-c:a", "aac", outputFilePath
+      };
 
-            AttachedFile attachedFile = new AttachedFile();
-            attachedFile.setContentType("mp4");
-            attachedFile.setSavePath(outputFilePath);
-            AttachedFile savedAttachedFile = attachedFileRepository.save(attachedFile);
+      ProcessBuilder pb = new ProcessBuilder(ffmpegCommand);
+      pb.inheritIO().start().waitFor();
 
-            VideoBlock videoBlock = new VideoBlock();
-            videoBlock.setTitle(originalFileName);
+      AttachedFile attachedFile = new AttachedFile();
+      attachedFile.setContentType("mp4");
+      attachedFile.setSavePath(outputFilePath);
+      AttachedFile savedAttachedFile = attachedFileRepository.save(attachedFile);
 
-            VideoBlockFile videoBlockFile = new VideoBlockFile();
-            videoBlockFile.setVideoBlock(videoBlock);
-            videoBlockFile.setAttachedFile(savedAttachedFile);
-            VideoBlock savedVideoBlock = videoBlockRepository.save(videoBlock);
+      VideoBlock videoBlock = new VideoBlock();
+      videoBlock.modifyTitle(originalFileName);
 
-        } catch (IOException e) {
-            log.error("영상 업로드 중 오류 발생", e);
-        } catch (InterruptedException e) {
-            log.error("ffmpeg 프로세스 실행 중 오류 발생", e);
-        }
+      VideoBlockFile videoBlockFile = new VideoBlockFile();
+      videoBlockFile.setVideoBlock(videoBlock);
+      videoBlockFile.setAttachedFile(savedAttachedFile);
+      VideoBlock savedVideoBlock = videoBlockRepository.save(videoBlock);
+
+    } catch (IOException e) {
+      log.error("영상 업로드 중 오류 발생", e);
+    } catch (InterruptedException e) {
+      log.error("ffmpeg 프로세스 실행 중 오류 발생", e);
     }
+  }
 
-    //폴더 생성
-    private String generateFolderName(String originalFileName) {
+  // 폴더 생성
+  private String generateFolderName(String originalFileName) {
 
-        String folderName = originalFileName.replaceAll("[^a-zA-Z0-9]", "_");
-        return folderName;
-    }
+    String folderName = originalFileName.replaceAll("[^a-zA-Z0-9]", "_");
+
+    return folderName;
+  }
+
+  public void modifyVideoBlock(UUID pageId, UUID blockId, ModifyVideoBlock modifyVideoBlock) {}
+
+  public void deleteVideoBlock(UUID pageId, UUID blockId) {}
 }
