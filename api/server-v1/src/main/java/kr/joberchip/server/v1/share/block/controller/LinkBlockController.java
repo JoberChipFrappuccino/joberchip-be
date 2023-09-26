ec420ebb0edb1b0c0989e2a18e2e2a4529c4fb70
YongHo Shin <gdtknight@naver.com>
2023-09-26 14:06:10 +0900

    [api-server-v1] feat: LinkBlockController (#49)
    - URI 수정
    - Controller 에서 객체 생성하던 부분 삭제
    - 추후 Validation 필요

--- api/server-v1/src/main/java/kr/joberchip/server/v1/share/block/controller/LinkBlockController.java
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/share/block/controller/LinkBlockController.java
@@ -1,42 +1,45 @@
 package kr.joberchip.server.v1.share.block.controller;
 
-import kr.joberchip.core.space.block.LinkBlock;
+import java.util.UUID;
 import kr.joberchip.server.v1._utils.ApiResponse;
-import kr.joberchip.server.v1.space.block.dto.create.CreateLinkBlock;
-import kr.joberchip.server.v1.space.block.service.LinkBlockService;
+import kr.joberchip.server.v1.share.block.dto.create.CreateLinkBlock;
+import kr.joberchip.server.v1.share.block.dto.modify.ModifyLinkBlock;
+import kr.joberchip.server.v1.share.block.service.LinkBlockService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/space/{spaceId}/page/{pageId}/block")
+@RequestMapping("/v1/page/{pageId}/linkBlock")
 public class LinkBlockController {
   private final LinkBlockService linkBlockService;
 
-  @PostMapping("/link")
-  public ApiResponse.Result<Object> createLinkBlock(
-          @PathVariable Long spaceId,
-          @PathVariable Long pageId,
-          @RequestBody CreateLinkBlock createLinkBlock
-  ) {
-    String title = createLinkBlock.getTitle();
-    String description = createLinkBlock.getDescription();
-    String link = createLinkBlock.getLink();
+  @PostMapping
+  public ResponseEntity<ApiResponse.Result<Object>> createLinkBlock(
+      @PathVariable UUID pageId, @RequestBody CreateLinkBlock createLinkBlock) {
 
-    LinkBlock linkBlock = linkBlockService.createLinkBlock(title, description, link);
+    linkBlockService.createLinkBlock(pageId, createLinkBlock);
 
-    return ApiResponse.success(linkBlock);
+    return ResponseEntity.ok(ApiResponse.success());
   }
 
-  @PutMapping("/link/{blockId}")
+  @PutMapping("/{blockId}")
   public ApiResponse.Result<Object> modifyLinkBlock(
-      @PathVariable Long spaceId,
-      @PathVariable Long pageId,
+      @PathVariable UUID pageId,
       @PathVariable Long blockId,
-      @RequestBody CreateLinkBlock createLinkBlock) {
+      @RequestBody ModifyLinkBlock modifyLinkBlock) {
+
+    return ApiResponse.success();
+  }
+
+  @DeleteMapping("/{blockId}")
+  public ApiResponse.Result<Object> deleteLinkBlock(
+      @PathVariable UUID pageId, @PathVariable UUID blockId) {
+
+    linkBlockService.deleteLinkBlock(pageId, blockId);
 
     return ApiResponse.success();
   }
