96e590c4e376e0c2cbd128053936aa73bdfb5784
HanHyeJi <hjhan.0621@gmail.com>
2023-10-05 19:53:27 +0900

    [server] update: 이미지, 링크 블록 생성-수정-삭제 시 Controller에서 checkEditPrivilege() 실행

--- api/server-v1/src/main/java/kr/joberchip/server/v1/block/controller/ImageBlockController.java
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/block/controller/ImageBlockController.java
@@ -1,13 +1,17 @@
 package kr.joberchip.server.v1.block.controller;
 
 import java.util.UUID;
+
+import kr.joberchip.server.v1._config.security.CustomUserDetails;
 import kr.joberchip.server.v1._utils.ApiResponse;
 import kr.joberchip.server.v1.block.controller.dto.BlockResponseDTO;
 import kr.joberchip.server.v1.block.controller.dto.ImageBlockDTO;
 import kr.joberchip.server.v1.block.service.ImageBlockService;
+import kr.joberchip.server.v1.page.service.SharePagePrivilegeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -16,10 +20,15 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class ImageBlockController {
   private final ImageBlockService imageBlockService;
+  private final SharePagePrivilegeService sharePagePrivilegeService;
 
   @PostMapping
   public ResponseEntity<ApiResponse.Result<BlockResponseDTO>> createImageBlock(
-      @PathVariable UUID pageId, ImageBlockDTO imageBlockDTO) {
+          @AuthenticationPrincipal CustomUserDetails loginUser,
+          @PathVariable UUID pageId,
+          @RequestBody ImageBlockDTO imageBlockDTO) {
+
+    sharePagePrivilegeService.checkEditPrivilege(loginUser.user().getUserId(), pageId);
     BlockResponseDTO response = imageBlockService.createImageBlock(pageId, imageBlockDTO);
 
     return ResponseEntity.ok(ApiResponse.success(response));
@@ -27,10 +36,12 @@ public class ImageBlockController {
 
   @PutMapping("/{blockId}")
   public ResponseEntity<ApiResponse.Result<BlockResponseDTO>> modifyImageBlock(
-      @PathVariable UUID pageId,
-      @PathVariable UUID blockId,
-      ImageBlockDTO imageBlockDTO) {
+          @AuthenticationPrincipal CustomUserDetails loginUser,
+          @PathVariable UUID pageId,
+          @PathVariable UUID blockId,
+          @RequestBody ImageBlockDTO imageBlockDTO) {
 
+    sharePagePrivilegeService.checkEditPrivilege(loginUser.user().getUserId(), pageId);
     BlockResponseDTO response = imageBlockService.modifyImageBlock(pageId, blockId, imageBlockDTO);
 
     return ResponseEntity.ok(ApiResponse.success(response));
@@ -38,8 +49,11 @@ public class ImageBlockController {
 
   @DeleteMapping("/{blockId}")
   public ResponseEntity<ApiResponse.Result<Object>> deleteImageBlock(
-      @PathVariable UUID pageId, @PathVariable UUID blockId) {
+          @AuthenticationPrincipal CustomUserDetails loginUser,
+          @PathVariable UUID pageId,
+          @PathVariable UUID blockId) {
 
+    sharePagePrivilegeService.checkEditPrivilege(loginUser.user().getUserId(), pageId);
     imageBlockService.deleteImageBlock(pageId, blockId);
 
     return ResponseEntity.ok(ApiResponse.success());
--- api/server-v1/src/main/java/kr/joberchip/server/v1/block/controller/LinkBlockController.java
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/block/controller/LinkBlockController.java
@@ -1,13 +1,17 @@
 package kr.joberchip.server.v1.block.controller;
 
 import java.util.UUID;
+
+import kr.joberchip.server.v1._config.security.CustomUserDetails;
 import kr.joberchip.server.v1._utils.ApiResponse;
 import kr.joberchip.server.v1.block.controller.dto.BlockResponseDTO;
 import kr.joberchip.server.v1.block.controller.dto.LinkBlockRequestDTO;
 import kr.joberchip.server.v1.block.service.LinkBlockService;
+import kr.joberchip.server.v1.page.service.SharePagePrivilegeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -16,11 +20,15 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/v1/page/{pageId}/linkBlock")
 public class LinkBlockController {
   private final LinkBlockService linkBlockService;
+  private final SharePagePrivilegeService sharePagePrivilegeService;
 
   @PostMapping
   public ResponseEntity<ApiResponse.Result<Object>> createLinkBlock(
-      @PathVariable UUID pageId, @RequestBody LinkBlockRequestDTO createLinkBlock) {
+      @AuthenticationPrincipal CustomUserDetails loginUser,
+      @PathVariable UUID pageId,
+      @RequestBody LinkBlockRequestDTO createLinkBlock) {
 
+    sharePagePrivilegeService.checkEditPrivilege(loginUser.user().getUserId(), pageId);
     BlockResponseDTO responseDTO = linkBlockService.createLinkBlock(pageId, createLinkBlock);
 
     return ResponseEntity.ok(ApiResponse.success(responseDTO));
@@ -28,10 +36,12 @@ public class LinkBlockController {
 
   @PutMapping("/{blockId}")
   public ResponseEntity<ApiResponse.Result<Object>> modifyLinkBlock(
+      @AuthenticationPrincipal CustomUserDetails loginUser,
       @PathVariable UUID pageId,
       @PathVariable UUID blockId,
       @RequestBody LinkBlockRequestDTO modifyRequestDTO) {
 
+    sharePagePrivilegeService.checkEditPrivilege(loginUser.user().getUserId(), pageId);
     BlockResponseDTO responseDTO = linkBlockService.modifyLinkBlock(pageId, blockId, modifyRequestDTO);
 
     return ResponseEntity.ok(ApiResponse.success(responseDTO));
@@ -39,8 +49,10 @@ public class LinkBlockController {
 
   @DeleteMapping("/{blockId}")
   public ResponseEntity<ApiResponse.Result<Object>> deleteLinkBlock(
+      @AuthenticationPrincipal CustomUserDetails loginUser,
       @PathVariable UUID pageId, @PathVariable UUID blockId) {
 
+    sharePagePrivilegeService.checkEditPrivilege(loginUser.user().getUserId(), pageId);
     linkBlockService.deleteLinkBlock(pageId, blockId);
 
     return ResponseEntity.ok(ApiResponse.success());
