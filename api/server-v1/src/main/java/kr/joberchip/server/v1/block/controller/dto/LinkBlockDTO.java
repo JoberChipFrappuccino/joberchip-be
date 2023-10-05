b5f3f80409890114302bf3052130260231ff51b0
YongHo Shin <gdtknight@naver.com>
2023-10-05 23:00:03 +0900

    [api-server-v1] fix: DTO 어노테이션 정리

--- api/server-v1/src/main/java/kr/joberchip/server/v1/block/controller/dto/LinkBlockDTO.java
+++ api/server-v1/src/main/java/kr/joberchip/server/v1/block/controller/dto/LinkBlockDTO.java
@@ -1,16 +1,9 @@
 package kr.joberchip.server.v1.block.controller.dto;
 
 import kr.joberchip.core.block.LinkBlock;
-import org.springframework.web.bind.annotation.RequestParam;
 
 public record LinkBlockDTO(
-    @RequestParam String title,
-    @RequestParam String link,
-    @RequestParam Integer x,
-    @RequestParam Integer y,
-    @RequestParam Integer w,
-    @RequestParam Integer h,
-    @RequestParam Boolean visible) {
+    String title, String link, Integer x, Integer y, Integer w, Integer h, Boolean visible) {
 
   public LinkBlock toEntity() {
     return LinkBlock.of(title, link, x, y, w, h);
